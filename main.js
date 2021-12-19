806b277f6c7c9c13977bc97bef9e7828db8dcc79
Levi <longlevi6@gmail.com>
2021-12-19 08:32:58 -0600

    Added 3 arrays of fortunes to randomize

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-//in progress
+const fortune1 = ['be on the alert to recognize your prime', 'happiness is not an outside job,', 'your road to glory will be rocky', 'no holidays', 
+                  'he who dies with most toys', 'you will be hungry again in one hour', 'you do not need strength', 'the real kindness comes from within you', 
+                  'courage is not simply one of the virtues', 'the older you get, the better you were.'];
+
+const fortune2 = ['but the form of every virtue at the testing point', 'big journeys begin', 'if you want the rainbow, ', 
+                  'but the second mouse gets the cheese', 'easiest way to find lost object', 'age is high price', 'abort, retry, ignore', 
+                  'only outlaws have in-laws', 'fat person not afraid of heights...afraid of widths', 'practice safe eating Always use condiments'];
+
+const fortune3 = ['makes you think that he is dumb', 'be not afraid of growing slowly,', 'cup is in its emptiness', 'just do not stare', 
+                  'he who laughs last ', 'One tequila, two tequila, three tequila, floor', 'food poisoning no fun', 'only difference between rut and a grave is depth', 
+                  'get out of the way', 'big journeys begin with a single step']; 
